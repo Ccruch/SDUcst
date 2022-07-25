27ecaea0ddb0e5bfbba5d377cec70f700ba087a4
Ccruch <105582476+Ccruch@users.noreply.github.com>
2022-07-25 20:54:42 +0800

    Delete SM3 directory

--- SM3/for_optimize.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define hash_size 32  // 32*8=256-bit
-namespace SM3 {
-    typedef struct Context {
-        unsigned int in_me_HASH[hash_size / 4];
-        unsigned char message_block[64];  // 512-bit msg block
-    }Context;
-}
